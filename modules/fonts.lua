56b3af57948183edace6ce69c6e6c5fde16426d6
ipufo1 <116466030+ipufo1@users.noreply.github.com>
2025-04-26 23:48:07 -0700

    Update fonts.lua

--- modules/fonts.lua
+++ modules/fonts.lua
@@ -1,34 +1,52 @@
-local HttpService = game:GetService('HttpService');
+local foldername = ...;
+
+local HttpService = cloneref(game:GetService('HttpService'));
 local Fonts = {
     _registry = {};
 };
 
-if not isfolder('twinkhook/fonts') then
-    makefolder('twinkhook/fonts');
+if not isfolder(foldername.. '/fonts') then
+    makefolder(foldername.. '/fonts');
+end;
+
+local _, ver = identifyexecutor();
+if not isfile(foldername.. '/version.txt') then
+    writefile(foldername.. '/version.txt', ver);
+end;
+
+if readfile(foldername.. '/version.txt') ~= ver then
+    delfolder(foldername.. '/fonts');
+    delfile(foldername.. '/version.txt'); writefile(foldername.. '/version.txt', ver);
+end;
+
+if not isfolder(foldername.. '/fonts') then
+    makefolder(foldername.. '/fonts');
 end;
 
 function Fonts.Append(Name, Data, ENCODED)
-    local Path = `twinkhook/fonts/{Name}`;
+    local Path = foldername.. `/fonts/{Name}`;
 
     if not isfile(`{Path}.ttf`) then
         writefile(`{Path}.ttf`, (ENCODED and base64.decode(Data) or Data));
     end;
 
-    if not isfile(`{Path}.json`) then
-        local FontData = HttpService:JSONEncode({
-            ['name'] = Name;
-            ['faces'] = {{
-                ['name'] = 'Regular';
-                ['style'] = 'normal';
-                ['weight'] = 400;
-                ['assetId'] = getcustomasset(`{Path}.ttf`);
-            }}
-        })
-
-        writefile(`{Path}.json`, FontData);
+    if isfile(`{Path}.json`) then
+        delfile(`{Path}.json`);
     end;
 
-    return Font.new(getcustomasset(`{Path}.json`));
+    local FontData = HttpService:JSONEncode({
+        ['name'] = Name;
+        ['faces'] = {{
+            ['name'] = 'Regular';
+            ['style'] = 'normal';
+            ['weight'] = 400;
+            ['assetId'] = getcustomasset(`{Path}.ttf`);
+        }}
+    })
+
+    writefile(`{Path}.json`, FontData);
+
+    return Font.new(getcustomasset(`{Path}.json`), Enum.FontWeight.Regular, Enum.FontStyle.Normal);
 end;
 
 return Fonts;
